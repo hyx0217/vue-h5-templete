5899437f0492f620390f8cbdd3d481ddba7941d1
unknown <changan@MYun.info>
2020-07-16 15:57:02 +0800

    update：优化打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,23 @@
-const path = require('path')
+const path = require("path");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
+const isProd = process.env.NODE_ENV === "production";
 function resolve(dir) {
-  return path.join(__dirname, dir)
+  return path.join(__dirname, dir);
 }
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/web" : "/",
-  configureWebpack: {
-    name: 'vue3',
-    resolve: {
-      alias: {
-        '@': resolve('src')
-      }
+  configureWebpack: (config) => {
+    config.resolve.alias.set("@", resolve("src"));
+    /* gzip压缩，nginx需要开启gzip*/
+    if (isProd) {
+      // 配置webpack 压缩
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          test: /\.js$|\.html$|\.css$/,
+          // 超过4kb压缩
+          threshold: 4096,
+        })
+      );
     }
   },
   devServer: {
@@ -17,21 +25,21 @@ module.exports = {
     https: false,
     proxy: {
       // GitHub请求token的请求
-      '/githubAccessToken': {
-        target: 'https://github.com/login/oauth/access_token',
+      "/githubAccessToken": {
+        target: "https://github.com/login/oauth/access_token",
         changeOrigin: true,
         pathRewrite: {
-          '^/githubAccessToken': '',
+          "^/githubAccessToken": "",
         },
       },
       // GitHub请求token的请求
-      '/githubUserInfo': {
-        target: 'https://api.github.com/user',
+      "/githubUserInfo": {
+        target: "https://api.github.com/user",
         changeOrigin: true,
         pathRewrite: {
-          '^/githubUserInfo': '',
+          "^/githubUserInfo": "",
         },
       },
     },
   },
-}
+};
