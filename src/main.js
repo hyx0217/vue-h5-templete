c1133ce8d7b02933b7af23a4b7d819ed1145144a
unknown <changan@MYun.info>
2020-07-16 15:39:04 +0800

    add:图片懒加载设置默认图片

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { getToken } from "./utils/auth";
+// import { Lazyload } from 'vant';
 
 Vue.config.productionTip = false;
+/* 图片懒加载，设置默认图片 */
+/* Vue.use(Lazyload,{
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: 'dist/loading.gif',
+  attempt: 1
+}) */
 router.beforeEach((to, from, next) => {
   //白名单路由
   const whiteUrl = ["/login", "/forget", "/register", "/signin_github"];
