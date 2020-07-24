b6557a866adf899b4875667f404ca3d0260f3c3b
changan <>
2020-07-24 10:12:35 +0800

    update:新增打包cdn优化

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,39 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/web" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   chainWebpack: (config) => {
+    // 关闭预加载
+    //  config.plugins.delete('prefetch');
+    //  config.plugins.delete('preload');
     config.resolve.alias.set("@", resolve("src"));
+    // 忽略生成环境打包的文件
+    var externals = {
+      vue: "Vue",
+      axios: "axios",
+      "vue-router": "VueRouter",
+      vuex: "Vuex",
+      // vant: 'Vant'
+    };
+    config.externals(externals);
+    // cdn可替换成自己服务器上的，更加稳定
+    const cdn = {
+      css: [],
+      js: [
+        // vue
+        "https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js",
+        // vue-router
+        "https://cdn.jsdelivr.net/npm/vue-router/dist/vue-router.min.js",
+        // vuex
+        "https://cdn.jsdelivr.net/npm/vuex/dist/vuex.min.js",
+        // axios
+        "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",
+      ],
+    };
+    config.plugin("html").tap((args) => {
+      args[0].cdn = cdn;
+      return args;
+    });
   },
   configureWebpack: (config) => {
     /* gzip压缩，nginx需要开启gzip*/
